d17ed8be98ed2850e8c10d852aa8b4dc13dc81fb
Charles Findlay <cfindl10@ford.com>
2020-11-10 11:24:32 -0500

    started stopwatch code

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
-import {from} from 'rxjs'
-import {
-  distinctUntilKeyChanged,
-  map,
-  scan,
-  tap,
-} from 'rxjs/operators'
+import {fromEvent, interval} from 'rxjs'
+import {switchMap} from 'rxjs/operators'
 
 const log = console.log
+const startButton = document.querySelector('#start')
 
-const users = [
-  {name: 'Brian', loggedIn: false, token: null},
-  {name: 'Brian', loggedIn: true, token: 'abc'},
-  {name: 'Brian', loggedIn: true, token: '123'},
-]
-
-const state$ = from(users).pipe(
-  scan((acc, curr) => {
-    return {...acc, ...curr}
-  }, {}),
-)
-
-const name$ = state$.pipe(
-  distinctUntilKeyChanged('name'),
-  tap(log),
-  map(state => state.name),
-)
-
-name$.subscribe(log)
+fromEvent(startButton, 'click')
+  .pipe(switchMap(x => interval(1000)))
+  .subscribe(x => console.log(x))
